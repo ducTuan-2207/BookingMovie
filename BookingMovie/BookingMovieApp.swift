232d7fb82cbfbeb140021039a002883a1e60dca6
DucTuan <tuanhypc02@gmail.com>
2024-04-07 22:30:18 +0700

    Initial Commit

---
+++ BookingMovie/BookingMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookingMovieApp.swift
+//  BookingMovie
+//
+//  Created by macOS on 07/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookingMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
