b0c5a1b9cf8d2c9385c7b5b4b0a72766130dd7df
DucTuan <tuanhypc02@gmail.com>
2024-04-08 10:49:04 +0700

    data

---
+++ BookingMovie/Data/TabData.swift
@@ -0,0 +1,15 @@
+//
+//  TabData.swift
+//  BookingMovie
+//
+//  Created by macOS on 08/04/2024.
+//
+
+import Foundation
+enum Tab: String, CaseIterable {
+    case home = "Home"
+    case location = "Location"
+    case ticket = "Ticket"
+    case category = "Category"
+    case profile = "Profile"
+}
