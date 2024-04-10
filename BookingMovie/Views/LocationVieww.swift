a4fcbcb91564d4d3f81f283202e87d7558ae508f
DucTuan <tuanhypc02@gmail.com>
2024-04-10 21:22:10 +0700

    add view

---
+++ BookingMovie/Views/LocationVieww.swift
@@ -0,0 +1,20 @@
+//
+//  LocationVieww.swift
+//  BookingMovie
+//
+//  Created by macOS on 10/04/2024.
+//
+
+import SwiftUI
+
+struct LocationVieww: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationVieww_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationVieww()
+    }
+}
